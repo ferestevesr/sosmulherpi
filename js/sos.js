71a8d4f8034ef050205e43dcf189917a9f73f3a1
ferestevesr <fernandovinirocha@gmail.com>
2026-05-13 14:12:01 -0300

    Meus ajustes antes de atualizar

--- js/sos.js
+++ js/sos.js
@@ -1,20 +1,42 @@
-const botao = document.getElementById("btnSOS");
+const btn = document.getElementById("btnSOS");
 const barra = document.getElementById("barra");
 const status = document.getElementById("status");
 
-botao.addEventListener("click", () => {
+const wrapper = document.getElementById("cardsWrapper");
 
+const loc = document.getElementById("loc");
+const contatos = document.getElementById("contatos");
+const ajuda = document.getElementById("ajuda");
+const geral = document.getElementById("geral");
+
+let ativo = false;
+
+btn.addEventListener("click", () => {
+  if (ativo) return;
+  ativo = true;
+
+  // ATIVA SEGUNDO CARD
+  wrapper.classList.add("ativo");
+
+  // ETAPA 1
   status.innerHTML = "Enviando localização...";
   barra.style.width = "30%";
+  loc.innerText = "🟡 Enviando...";
+  geral.innerText = "Ativo";
 
   setTimeout(() => {
-    status.innerHTML = "Contato de emergência acionado";
-    barra.style.width = "70%";
+    // ETAPA 2
+    status.innerHTML = "Notificando contatos...";
+    barra.style.width = "60%";
+    loc.innerText = "🟢 Enviado";
+    contatos.innerText = "2 contatos ✔";
   }, 1500);
 
   setTimeout(() => {
-    status.innerHTML = "Ajuda a caminho ✔️";
+    // ETAPA 3
+    status.innerHTML = "Ajuda a caminho 🚨";
     barra.style.width = "100%";
+    ajuda.innerText = "A caminho ✔";
+    geral.innerText = "Finalizado";
   }, 3000);
-
 });
